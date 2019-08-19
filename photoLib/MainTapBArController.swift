eaadec1a8e05ef67f86a9be05053fa033cae4e1f
summjai <summjai@gmail.com>
2019-08-19 13:29:37 +0300

    add nav vc, two bar items photos and favourites

--- photoLib/MainTapBArController.swift
+++ photoLib/MainTapBArController.swift
@@ -11,7 +11,24 @@ import UIKit
 class MainTapBarController: UITabBarController {
 
     override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .blue
+
+        let photosCV = PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+
+        viewControllers = [
+            geneateNavigationController(rootViewController: photosCV, title: "Photos", image: #imageLiteral(resourceName: "photos")),
+            geneateNavigationController(rootViewController: ViewController(), title: "Favourite", image: #imageLiteral(resourceName: "heart"))
+        ]
+    }
+
+    private func geneateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
+
     }
 
 
---
+++ photoLib/PhotosCollectionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosCollectionViewController.swift
+//  photoLib
+//
+//  Created by Anastasiia Gachkovskaya on 19/08/2019.
+//  Copyright © 2019 Anastasia Gachkovskaya. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.backgroundColor = .orange
+        
+
+    }
+
+
+
+
+}
