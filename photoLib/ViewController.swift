69471eb7d49758dcc78b93aacf85ed9d512a35ce
summjai <summjai@gmail.com>
2019-08-19 12:02:53 +0300

    add gitignore

---
+++ photoLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoLib
+//
+//  Created by Anastasiia Gachkovskaya on 18/08/2019.
+//  Copyright © 2019 Anastasia Gachkovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
