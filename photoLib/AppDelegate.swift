c3645f45eb6dfe5be89a32cbc5a2aa6bcda1a6bd
summjai <summjai@gmail.com>
2019-08-19 12:25:52 +0300

    add window to appdel

--- photoLib/AppDelegate.swift
+++ photoLib/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        window = UIWindow()
+        window?.makeKeyAndVisible()
         window?.rootViewController = MainTapBarController()
 
         return true
