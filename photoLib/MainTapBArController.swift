eea7b48a8a3129d0338c9b4d19a66f4ace300ee2
summjai <summjai@gmail.com>
2019-08-19 12:18:14 +0300

    add mainTapBarController

---
+++ photoLib/MainTapBArController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTapBarController.swift
+//  photoLib
+//
+//  Created by Anastasiia Gachkovskaya on 19/08/2019.
+//  Copyright © 2019 Anastasia Gachkovskaya. All rights reserved.
+//
+
+import UIKit
+
+class MainTapBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .blue
+    }
+
+
+}
